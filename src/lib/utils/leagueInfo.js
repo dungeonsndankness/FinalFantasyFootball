b05b8e8feff28eea1297d5bf8757f1cb43c1ba0f
dungeonsndankness <78679877+dungeonsndankness@users.noreply.github.com>
2026-02-01 15:04:35 -0500

    Update leagueInfo.js
    Updated Sleeper league ID to reflect roll over to new league year

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1191060237150384128"; // your league ID
+export const leagueID = "1313929892500373504"; // your league ID
 export const leagueName = "Final Fantasy Football"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
